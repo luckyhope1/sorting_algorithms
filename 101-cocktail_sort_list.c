0011c4c5e629d68104ff3df9d67903b23ea8c3f0
Pinkypre <preciousPncube@gmail.com>
2023-07-03 14:46:53 -0700

    101-cocktail_sort_list.c

--- 101-cocktail_sort_list.c
+++ 101-cocktail_sort_list.c
@@ -1,66 +1,64 @@
 #include "sort.h"
 
 /**
-* swap - swaps two nodes
-* @head: head of the list
-* @node1: first node to sort
-* @node2: second node to sort
-*/
+ * swap - swaps two nodes
+ * @head: head of the list
+ * @node1: first node to sort
+ * @node2: second node to sort
+ */
 void swap(listint_t **head, listint_t *node1, listint_t *node2)
 {
-listint_t *prev, *next;
+	listint_t *prev, *next;
 
-prev = node1-&gt;prev;
-next = node2-&gt;next;
+	prev = node1->prev;
+	next = node2->next;
 
-if (prev != NULL)
-prev-&gt;next = node2;
-else
-*head = node2;
-
-node1-&gt;prev = node2;
-node1-&gt;next = next;
-node2-&gt;prev = prev;
-node2-&gt;next = node1;
-if (next)
-next-&gt;prev = node1;
+	if (prev != NULL)
+		prev->next = node2;
+	else
+		*head = node2;
+	node1->prev = node2;
+	node1->next = next;
+	node2->prev = prev;
+	node2->next = node1;
+	if (next)
+		next->prev = node1;
 }
 /**
-* cocktail_sort_list - sorts a list using the cocktail sort algorithm
-* @list: list to sort
-*/
+ * cocktail_sort_list - sorts a list using the cocktail sort algorithm
+ * @list: list to sort
+ */
 void cocktail_sort_list(listint_t **list)
 {
-listint_t *head;
-int flag = 0;
+	listint_t *head;
+	int flag = 0;
 
-if (!list || !*list || !(*list)-&gt;next)
-return;
+	if (!list || !*list || !(*list)->next)
+		return;
 
-do {
-for (head = *list; head-&gt;next != NULL; head = head-&gt;next)
-{
-if (head-&gt;n &gt; head-&gt;next-&gt;n)
-{
-swap(list, head, head-&gt;next);
-print_list(*list);
-flag = 1;
-head = head-&gt;prev;
-}
-
-}
-if (flag == 0)
-break;
-flag = 0;
-for (; head-&gt;prev != NULL; head = head-&gt;prev)
-{
-if (head-&gt;n &lt; head-&gt;prev-&gt;n)
-{
-swap(list, head-&gt;prev, head);
-print_list(*list);
-flag = 1;
-head = head-&gt;next;
-}
-}
-} while (flag == 1);
+	do {
+		for (head = *list; head->next != NULL; head = head->next)
+		{
+			if (head->n > head->next->n)
+			{
+				swap(list, head, head->next);
+				print_list(*list);
+				flag = 1;
+				head = head->prev;
+			}
+		}
+		if (flag == 0)
+			break;
+		flag = 0;
+		for (; head->prev != NULL; head = head->prev)
+		{
+			if (head->n < head->prev->n)
+			{
+				swap(list, head->prev, head);
+				print_list(*list);
+				flag = 1;
+				head = head->next;
+			}
+		}
+	} while (flag == 1);
 }
